6d985e3dedf4326e438ac66c471a233ca923763b
liuji <liujiboy@163.com>
2013-09-10 21:01:48 +0800

    add 003

---
+++ 002/HelloWorld.java
@@ -0,0 +1,12 @@
+package demo;
+import javax.swing.JFrame;
+public class HelloWorld{
+	public static void main(String args[])
+	{
+		JFrame frame=new JFrame();
+		frame.setTitle("Hello World");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,400);
+		frame.setVisible(true);
+	}
+}
