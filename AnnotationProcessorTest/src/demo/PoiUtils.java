89ffa409ac5c45d698d43d11db5d1e346dd81066
Ji Liu <liujiboy@163.com>
2020-03-08 12:17:16 +0800

    修改bug

--- AnnotationProcessorTest/src/demo/PoiUtils.java
+++
@@ -1,7 +0,0 @@
-package demo;
-import my.Version;
-
-@Version(major = 1,minor = 1)
-public class PoiUtils {
-
-}
---
+++ AnnotationProcessorTest/src/demo/PointUtils.java
@@ -0,0 +1,7 @@
+package demo;
+import my.Version;
+
+@Version(major = 2,minor = -1)
+public class PointUtils {
+
+}
